9002f0f1dfb0a6b47ebbb8a8ebba261054d41d24
Muhammed Kızıldağ <mhmmd0536@gmail.com>
2025-07-08 11:46:08 +0300

    w

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,11 @@ import { avenirFont } from "./font";
 
 const Home = async () => {
 
-  const products = await fetch('https://product-list-renart-case-study-3vfrm5xif.vercel.app/api/products');
+  
 
 
   const _products = await products.json();
-
-
+  
   return (
     <div className="flex justify-center items-center h-screen flex-col w-full">
       <div className={`${avenirFont.className} text-[45px]`}>Product List</div>
